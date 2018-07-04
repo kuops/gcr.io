e9fec804d5af418854e7d9705df4ee307e191309
kuops <opshsy@gmail.com>
2018-07-04 15:16:21 +0800

    Update get_image.sh

--- get_image.sh
+++ get_image.sh
@@ -120,7 +120,7 @@ image_push() {
                 tag_push &
                 let PROGRESS_COUNT++
             fi
-            COUNT_WAIT=$[$PROGRESS_COUNT%30]
+            COUNT_WAIT=$[$PROGRESS_COUNT%20]
             if [ $COUNT_WAIT -eq 0 ];then
                 wait
                clean_images
